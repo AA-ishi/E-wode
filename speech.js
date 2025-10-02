dfbf36d8337eb24ec5a6c8dfe6f5ed1995729e84
AA-ishi <ishidasako@yahoo.co.jp>
2025-10-02 14:14:37 +0900

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,4 +1,3 @@
-
 function waitForVoices(callback) {
   const voices = speechSynthesis.getVoices();
   if (voices.length > 0) {
@@ -10,17 +9,17 @@ function waitForVoices(callback) {
   }
 }
 
-
-export async function speak(text) {
+function speak(text) {
   if (!('speechSynthesis' in window)) return;
 
-  const voices = await waitForVoices();
-  const utter = new SpeechSynthesisUtterance(text);
-  utter.lang = 'en-US';
+  waitForVoices((voices) => {
+    const utter = new SpeechSynthesisUtterance(text);
+    utter.lang = 'en-US';
 
-  const enVoice = voices.find(v => v.lang.startsWith('en') && v.name.includes('Google'));
-  if (enVoice) utter.voice = enVoice;
+    const enVoice = voices.find(v => v.lang.startsWith('en') && v.name.includes('Google'));
+    if (enVoice) utter.voice = enVoice;
 
-  speechSynthesis.cancel();
-  speechSynthesis.speak(utter);
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utter);
+  });
 }
